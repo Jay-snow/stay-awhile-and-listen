01cefe6fd52a0e3c997add1a5b6909a6d2ec6e19
Jay-snow <marcusjsnow1@gmail.com>
2019-09-23 18:32:40 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Grid msg="Stay a while and listen"/>
+
+
+
+  </div>
+</template>
+
+<script>
+import Grid from './components/Grid.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Grid
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Grid.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+      <div id="board">
+        <table>
+          <tr v-for="block in grid">
+              <td> {{ grid }} </td>
+          </tr>
+      
+    </table>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'Grid',
+  props: {
+    msg: String,
+    
+
+  },
+  data: function() { 
+      return {
+    grid: [ [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0] ],
+    }
+  },
+  methods:{
+        log: function() {
+      console.log(this.grid);
+      } 
+  },
+
+  created: function(){
+    this.log();
+ 
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+#board {
+  display:flex;
+  justify-content: center;
+}
+
+#board table {
+  font-size:30px;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
